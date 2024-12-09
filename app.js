738b213a95fdc032ace139a35063a8997d76df63
mxqle <24hs216@s.bgu.ac.jp>
2024-12-09 14:08:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 
         function checkFortune() {
-            var number = document.getElementById('colour-select').value;
-            var gender = parseInt(document.getElementById('number-input').value, 10);
+            var gender = document.getElementById('colour-select').value;
+            var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "動物"; // デフォルトの値
 
-            if (number === "1" && gender === 女) {
+            if (gender === "女" && number === 1) {
                 result = "犬タイプ";
-            } else if (number === "1" && gender === 男) {
+            } else if (gender === "男" && number === 1) {
                 result = "キツネタイプ";
-            } else if (number === "2" && gender === 女) {
+            } else if (gender === "女" && number === 2) {
                 result = "猫タイプ";
-            } else if (number === "2" && gender === 男) {
+            } else if (gender === "男" && number === 2) {
                 result = "犬タイプ";
-            } else if (number === "3" && gender === 女) {
+            } else if (gender === "女" && number === 3) {
                 result = "くまタイプ";
-            } else if (number === "3" && gender === 男) {
+            } else if (gender === "男" && number === 3) {
                 result = "猫タイプ";
-            } else if (number === "4" && gender === 女) {
+            } else if (gender === "女" && number === 4) {
                 result = "うさぎタイプ";
-            } else if (number === "4" && gender === 男) {
+            } else if (gender === "男" && number === 4) {
                 result = "トラタイプ";
-            } else if (number === "5" && gener === 女) {
+            } else if (gender === "女" && number === 5) {
                 result = "キツネタイプ";
-            } else if (number === "5" && gener === 男) {
+            } else if (gender === "男" && number === 5) {
                 result = "うさぎタイプ";
-　　　　　　} else if (number === "6" && gener === 女) {
+　　　　　　} else if (gender === "女" && number === 6) {
                 result = "ひつじタイプ";
-　　　　　　} else if (number === "6" && gener === 男) {
+　　　　　　} else if (gender === "男" && number === 6) {
                 result = "くまタイプ";
 
             document.getElementById('result-output').innerText = "あなたの動物のタイプは？【" + result + "】です";
