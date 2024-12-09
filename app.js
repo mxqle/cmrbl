4f2abb2d326e5339ee9e33bfc31f3a0feb08c4fd
mxqle <24hs216@s.bgu.ac.jp>
2024-12-09 14:00:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
+
         function checkFortune() {
-            var colour = document.getElementById('colour-select').value;
-            var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "凶"; // デフォルトの値
+            var number = document.getElementById('colour-select').value;
+            var gender = parseInt(document.getElementById('number-input').value, 10);
+            var result = "動物"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
-            }
+            if (number === "1" && gender === 女) {
+                result = "犬タイプ";
+            } else if (number === "1" && gender === 男) {
+                result = "キツネタイプ";
+            } else if (number === "2" && gender === 女) {
+                result = "猫タイプ";
+            } else if (number === "2" && gender === 男) {
+                result = "犬タイプ";
+            } else if (number === "3" && gender === 女) {
+                result = "くまタイプ";
+            } else if (number === "3" && gender === 男) {
+                result = "猫タイプ";
+            } else if (number === "4" && gender === 女) {
+                result = "うさぎタイプ";
+            } else if (number === "4" && gender === 男) {
+                result = "トラタイプ";
+            } else if (number === "5" && gener === 女) {
+                result = "キツネタイプ";
+            } else if (number === "5" && gener === 男) {
+                result = "うさぎタイプ";
+　　　　　　} else if (number === "6" && gener === 女) {
+                result = "ひつじタイプ";
+　　　　　　} else if (number === "6" && gener === 男) {
+                result = "くまタイプ";
 
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+            document.getElementById('result-output').innerText = "あなたの動物のタイプは？【" + result + "】です";
         }
