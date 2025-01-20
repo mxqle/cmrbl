74d3b2502825c43dcb08992a8206ed84369175d8
mxqle <24hs216@s.bgu.ac.jp>
2025-01-20 13:45:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
         function checkFortune() {
-            var gender = document.getElementById('gender-select').value;
+            var color = document.getElementById('color-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "動物"; // デフォルトの値
 
-            if (gender === "女" && number === 1) {
+            if (color === "青" && number === 1) {
                 result = "犬タイプ";
-            } else if (gender === "男" && number === 1) {
+            } else if (color === "赤" && number === 1) {
                 result = "キツネタイプ";
-            } else if (gender === "女" && number === 2) {
+            } else if (color === "緑" && number === 1) {
                 result = "猫タイプ";
-            } else if (gender === "男" && number === 2) {
+            } else if (color === "青" && number === 2) {
                 result = "犬タイプ";
-            } else if (gender === "女" && number === 3) {
+            } else if (color === "赤" && number === 2) {
                 result = "くまタイプ";
-            } else if (gender === "男" && number === 3) {
+            } else if (color === "緑" && number === 2) {
                 result = "猫タイプ";
            }
         
